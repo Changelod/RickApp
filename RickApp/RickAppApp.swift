6512aba976218fb7b5b3ba59a75e09b0acb9ca2f
changelod <angelodcp@outlook.com.pe>
2024-01-20 03:30:58 -0500

    first commit

---
+++ RickApp/RickAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAppApp.swift
+//  RickApp
+//
+//  Created by Angelo Chavez on 20/01/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
